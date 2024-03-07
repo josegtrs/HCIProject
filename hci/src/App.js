75e958d1e6809c418ad006f3e3e17f0962ab7f49
Jose Saldivar <jose.a.saldivar-1@ou.edu>
2024-03-07 13:21:31 -0600

    basic home page just for visualization, no functionality

---
+++ hci/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { HomePage } from './Components/HomePage/HomePage'
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
