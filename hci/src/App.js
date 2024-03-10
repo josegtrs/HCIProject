c04ac085689ca6b8166218ac34748feae6e6aa3b
Xavier Williams <xkw.1602@gmail.com>
2024-03-10 17:54:15 -0500

    Added finish page template

--- hci/src/App.js
+++ hci/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './Components/HomePage/HomePage'
+import { FinishPage } from './Components/FinishPage/FinishPage'
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <FinishPage/>
     </div>
   );
 }
---
+++ hci/src/Components/FinishPage/FinishPage.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './FinishPage.css'
+
+export const FinishPage = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Thank You!</div>
+            <div className="underline"></div>
+        </div>
+        <div>
+            <div className='dialog'>Your vote has been recorded.</div>
+            <div className='dialog'>Please click the button below to return to the home page.</div>
+        </div>
+        <div className="submit-container">
+            <div className='submit' onClick={()=>{}}>Home Page</div>
+            
+        </div>
+    </div>
+  )
+}
