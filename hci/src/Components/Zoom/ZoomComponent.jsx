a00ac7876fd4318c447b4c92db001d64de57bf73
eidzaben24 <eidzaben24@gmail.com>
2024-04-26 20:35:33 -0500

    style changes

--- hci/src/Components/Zoom/ZoomComponent.jsx
+++ hci/src/Components/Zoom/ZoomComponent.jsx
@@ -4,8 +4,8 @@ import '../CSS/Global.css'
 const ZoomComponent = ({ zoomIn, zoomOut }) => {
   return (
     <div className="zoom-container">
+    <button className="zoom-button zoom-out" onClick={zoomOut}></button>
       <button className="zoom-button zoom-in" onClick={zoomIn}></button>
-      <button className="zoom-button zoom-out" onClick={zoomOut}></button>
     </div>
   );
 };
